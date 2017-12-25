ea7b7db27852c72049b4465bdf7a8518b249f57b
Andrey Voloshchuk <a.voloshchuk23@gmail.com>
2017-12-25 19:09:06 +0200

    JS add

--- js/app.js
+++ js/app.js
@@ -19,3 +19,66 @@ function menu(){
 	});			
 };
 menu();
+
+function slider(){
+	var prev = $(".slider .slider_wrap .prev"),
+			next = $(".slider .slider_wrap .next"),
+			item_width = $(".slider").width(),
+			item_height = $(".slider").height(),
+			marginLeft = 0,
+			item_counter = $(".slider .slider_wrap .slider_item").length;
+	$(window).resize(function(){
+		item_width = $(".slider").width();
+		item_height = $(".slider").height();
+		marginLeft = $(".slider").width();
+		$('.slider .slider_wrap .slider_item').width($(".slider").width());
+		$(".slider .slider_wrap").css("margin-left", -marginLeft);		
+	});		
+	next.on("click", function(){
+		marginLeft += $(".slider").width();
+		$(".slider .slider_wrap").css("margin-left", -marginLeft);
+		if(marginLeft > $(".slider").width() * item_counter - 1){
+			$(".slider .slider_wrap").css("margin-left", 0);	
+			marginLeft = 0;
+		};
+	});
+	prev.on("click", function(){
+		marginLeft -= $(".slider").width();
+		console.log(marginLeft);	
+		if (marginLeft < 0){
+			console.log("true");
+			marginLeft = $(".slider").width() * (item_counter - 1);
+			$(".slider .slider_wrap").css("margin-left", -marginLeft);	
+			console.log(marginLeft);
+		}
+		else $(".slider .slider_wrap").css("margin-left", -marginLeft);
+	});	
+};
+slider();
+
+
+function photoGallery(){
+	$(".blog_others_photo_item").on("click", function(e){
+		e.stopPropagation();
+		var clone = $(this).clone(),
+				close = $(".blog_others_photo__big .close").clone();
+		$(".blog_others_photo__big").empty();
+		$(".blog_others_photo__big").append(clone);
+		$(".blog_others_photo__big").append(close);
+		$(".close").on("click", function(){
+			if ($(".blog_others_photo__big").is(":visible") == true){
+			$(".blog_others_photo__big").fadeOut();
+			$(".black").fadeOut();
+		}
+		});
+		$(".blog_others_photo__big").fadeIn();
+		$(".black").fadeIn();
+	});
+		$(".black").on("click", function(){
+			if ($(".blog_others_photo__big").is(":visible") == true){
+			$(".blog_others_photo__big").fadeOut();
+			$(".black").fadeOut();
+		};
+	});	
+};		
+photoGallery();
